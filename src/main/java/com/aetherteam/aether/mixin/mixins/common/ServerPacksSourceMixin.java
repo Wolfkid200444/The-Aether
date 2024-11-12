ce6b9a75ab35e4808a532bf6a5c67fc9c96acc81
Dragon-Seeker <timekeeperguild@gmail.com>
2024-11-11 19:28:59 -0600

    Fix datapacks not showing up within datapack world selection screen
    Fixes #2344

--- src/main/java/com/aetherteam/aether/mixin/mixins/common/ServerPacksSourceMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/ServerPacksSourceMixin.java
@@ -5,16 +5,19 @@ import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.server.packs.repository.ServerPacksSource;
 import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.validation.DirectoryValidator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.nio.file.Path;
+
 @Mixin(ServerPacksSource.class)
 public abstract class ServerPacksSourceMixin {
 
-    @Inject(method = "createPackRepository(Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;)Lnet/minecraft/server/packs/repository/PackRepository;", at = @At("RETURN"))
-    private static void aetherFabric$addServerPackResources(LevelStorageSource.LevelStorageAccess level, CallbackInfoReturnable<PackRepository> cir) {
+    @Inject(method = "createPackRepository(Ljava/nio/file/Path;Lnet/minecraft/world/level/validation/DirectoryValidator;)Lnet/minecraft/server/packs/repository/PackRepository;", at = @At("RETURN"))
+    private static void aetherFabric$addServerPackResources(Path folder, DirectoryValidator validator, CallbackInfoReturnable<PackRepository> cir) {
         AddPackFindersEvent.invokeEvent(PackType.SERVER_DATA, cir.getReturnValue());
     }
 
