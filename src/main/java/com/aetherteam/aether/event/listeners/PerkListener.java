7e0706e37f234fdb7b460e3edd3ca49fb5cd60e8
bconlon <bconlon.gml@gmail.com>
2024-11-13 18:07:27 -0800

    improv: fix moa skins not registering on client correctly

--- src/main/java/com/aetherteam/aether/event/listeners/PerkListener.java
+++ src/main/java/com/aetherteam/aether/event/listeners/PerkListener.java
@@ -2,8 +2,11 @@ package com.aetherteam.aether.event.listeners;
 
 import com.aetherteam.aether.Aether;
 import com.aetherteam.aether.event.hooks.PerkHooks;
+import com.aetherteam.aether.network.packet.clientbound.RegisterMoaSkinsPacket;
 import com.aetherteam.aether.perk.types.MoaSkins;
+import com.aetherteam.aetherfabric.network.PacketDistributor;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 
 public class PerkListener {
@@ -20,5 +23,8 @@ public class PerkListener {
     public static void playerLoggedIn(Player player) {
         PerkHooks.refreshPerks(player);
         MoaSkins.registerMoaSkins(player.level());
+        if (player instanceof ServerPlayer serverPlayer) {
+            PacketDistributor.sendToPlayer(serverPlayer, new RegisterMoaSkinsPacket());
+        }
     }
 }
