f5f2537bc0e565f6c77db40bd2c31a83194fb112
Dragon-Seeker <timekeeperguild@gmail.com>
2024-11-14 19:11:20 -0600

    fix: prevent sweeping attacks for Valk Lance

--- src/main/java/com/aetherteam/aether/mixin/mixins/common/PlayerMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/PlayerMixin.java
@@ -2,6 +2,7 @@ package com.aetherteam.aether.mixin.mixins.common;
 
 import com.aetherteam.aether.entity.passive.MountableAnimal;
 import com.aetherteam.aether.event.hooks.AbilityHooks;
+import com.aetherteam.aether.item.combat.loot.ValkyrieLanceItem;
 import com.aetherteam.aetherfabric.events.CancellableCallbackImpl;
 import com.aetherteam.aetherfabric.events.PlayerEvents;
 import com.aetherteam.aetherfabric.events.PlayerTickEvents;
@@ -9,16 +10,19 @@ import com.aetherteam.aether.mixin.AetherMixinHooks;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.player.PlayerModelPart;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.level.block.state.BlockState;
 import org.apache.commons.lang3.mutable.MutableFloat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -86,5 +90,10 @@ public class PlayerMixin {
         PlayerEvents.ON_BLOCK_DESTROY.invoker().onDestroy((Player) (Object) this, state, speed, callback);
 
         return callback.isCanceled() ? -1 : speed.getValue();
+    }
+
+    @WrapOperation(method = "attack", constant = @Constant(classValue = SwordItem.class))
+    private boolean aetherFabric$preventSweeping(Object object, Operation<Boolean> original) {
+        return original.call(object) && !(object instanceof ValkyrieLanceItem);
     }
 }
